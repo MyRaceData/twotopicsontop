a80e4b8adb3223791a8c7e94769d78d9cb26cc9e
Andrew Rowe <exectechnology@gmail.com>
2026-01-25 14:39:42 -0500

    Delete javascripts/discourse/initializers directory

--- javascripts/discourse/initializers/add-first-posts-to-topic-list.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import TopFirstPosts from "../components/top-first-posts";
-
-export default apiInitializer((api) => {
-  api.renderInOutlet("discovery-list-container-top", TopFirstPosts);
-});
