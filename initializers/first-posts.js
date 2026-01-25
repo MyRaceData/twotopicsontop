102da02b9c55050d40f481c53d4e8c6236ccf69e
Andrew Rowe <exectechnology@gmail.com>
2026-01-25 14:48:58 -0500

    Delete initializers directory

--- initializers/first-posts.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import FirstPosts from "discourse/components/first-posts"; // path depends on your theme structure
-
-export default apiInitializer((api) => {
-  api.renderInOutlet("discovery-list-container-top", FirstPosts);
-});
